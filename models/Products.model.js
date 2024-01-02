c1ec1e4cfd171e5b3481c396aa280dece5394fa9
ismayilhsnv <tu7u02d2s>
2024-01-03 02:55:56 +0400

    update

--- models/Products.model.js
+++ models/Products.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const Product = mongoose.model("Product", new mongoose.Schema({
-    title: String,
+    price: Number,
     desc: String,
     image: String
 }))
